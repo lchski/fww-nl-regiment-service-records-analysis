2cb3bc99aa7e8650adac30279243473037c12775
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-04-01 22:23:31 -0400

    moar stats

--- analysis/religion.R
+++ analysis/religion.R
@@ -1,38 +1,36 @@
 library(rapportools)
+library(DescTools)
 
 other_religions <- service_records %>%
   count_group(religion_cleaned) %>%
   filter(prop < 0.01) %>%
   pull(religion_cleaned)
 
-service_records %>%
-  mutate(religion_cleaned = ifelse(religion_cleaned %in% other_religions, "Other", religion_cleaned))
-
-service_records %>%
-  mutate(from_st_johns = id %in% st_johns_records_ids) %>%
-  count_group(from_st_johns, religion_cleaned) %>%
-  filter(count > 10) %>%
-  arrange(religion_cleaned, from_st_johns)
-
-service_records %>%
+area_of_origin__religion <- service_records %>%
+  mutate(religion_cleaned = ifelse(religion_cleaned %in% other_religions, "Other", religion_cleaned)) %>% ## regroup smaller religions under "other"
   mutate(area_of_origin = if_else(id %in% st_johns_records_ids, "St. John's", "Outside St. John's")) %>%
-  count_group(area_of_origin, religion_cleaned) %>%
-  arrange(religion_cleaned, area_of_origin) %>%
-  pivot_wider(
-    id_cols = religion_cleaned,
-    names_from = area_of_origin,
-    values_from = count,
-    values_fill = list(count = 0)
-  )
+  select(area_of_origin, religion_cleaned)
 
-area_of_origin__religion__xtab <- service_records %>%
-  mutate(area_of_origin = if_else(id %in% st_johns_records_ids, "St. John's", "Outside St. John's")) %>%
-  select(area_of_origin, religion_cleaned) %>%
-  table()
+area_of_origin__religion %>%
+  tabyl(religion_cleaned, area_of_origin) %>%
+  adorn_percentages(denominator = "col")
+
+area_of_origin__religion__xtab <- area_of_origin__religion %>%
+  table() %>%
+  t()
 
 area_of_origin__religion__xtab %>%
   chisq.test()
 
+area_of_origin__religion__xtab %>%
+  CramerV()
+
+area_of_origin__religion__xtab %>%
+  Lambda(direction = "symmetric")
+area_of_origin__religion__xtab %>%
+  Lambda(direction = "column")
+area_of_origin__religion__xtab %>%
+  Lambda(direction = "row")
 area_of_origin__religion__xtab %>%
   lambda.test()
 
